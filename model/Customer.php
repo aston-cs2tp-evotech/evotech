0f684b657471b9d7ac76a48e7121111ab8847b05
YourKalamity <kalam@kalam.dev>
2023-12-02 12:06:59 +0000

    Change parameter name to UID

--- model/Customer.php
+++ model/Customer.php
@@ -106,12 +106,12 @@ class CustomerModel {
     /**
      * Update a customer's details.
      *
-     * @param int $customerID The unique identifier of the customer.
+     * @param int $UID The unique identifier of the customer.
      * @param string $field The field to update.
      * @param string $value The new value.
      * @return bool True if the update is successful, false otherwise.
      */
-    public function updateCustomerDetail($customerID, $field, $value) {
+    public function updateCustomerDetail($UID, $field, $value) {
         // Validate $field to prevent SQL injection
         $allowedFields = ['Email', 'Username', 'CustomerAddress', 'PasswordHash'];
         if (!in_array($field, $allowedFields)) {
@@ -121,7 +121,7 @@ class CustomerModel {
         $query = "UPDATE `Customers` SET `$field` = :value WHERE `CustomerID` = :customerID";
         $statement = $this->database->prepare($query);
         $statement->bindParam(':value', $value, PDO::PARAM_STR);
-        $statement->bindParam(':customerID', $customerID, PDO::PARAM_INT);
+        $statement->bindParam(':customerID', $UID, PDO::PARAM_INT);
 
         try {
             return $statement->execute();
