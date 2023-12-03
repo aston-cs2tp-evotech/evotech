ffaadb864f798c2af7d3e39bba689f77075ba0b7
Hanzalah Naguthane <220070614@aston.ac.uk>
2023-12-03 18:59:33 +0000

    changed css file name to aboutus.css

--- view/aboutus.php
+++ view/aboutus.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Us</title>
-    <link rel="stylesheet" href="css/stylesheet.css">
+    <link rel="stylesheet" href="css/aboutus.css">
     
 </head>
 <body>
