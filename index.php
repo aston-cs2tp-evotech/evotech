36cd8bdf8601de3f1a494a73d23a31557c67e0fd
Kalam <kalam@kalam.dev>
2023-12-04 19:50:53 +0000

    Remove unnecessary HTML

--- index.php
+++ index.php
@@ -8,15 +8,15 @@ switch ($request) {
         require __DIR__ . '/view/home.php';
         break;
 
-    case '/view/aboutus':
+    case '/aboutus':
         require __DIR__ . '/view/aboutus.php';
         break;
 
-    case '/view/login':
+    case '/login':
         require __DIR__ . '/view/login.php';
         break;
 
-    case '/view/register':
+    case '/register':
         require __DIR__ . '/view/register.php';
         break;
     
@@ -30,22 +30,3 @@ include 'config/database.php';
 echo "<h1>evotech;</h1>";
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Evotech</title>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css.bootstrap.min.css">
-    <link rel="stylesheet" href="css/stylesheet.css">
-</head>
-<body>
-    <div class=
-
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js.bootstrap.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-</body>
-</html>
