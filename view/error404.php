6656516bb9fc706d1efc0800bdb230b42c184897
Kalam <kalam@kalam.dev>
2023-12-06 11:25:18 +0000

    Fix CSS linking

--- view/error404.php
+++ view/error404.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>404 - Page Not Found</title>
-    <link rel="stylesheet" href="css/error404.css">
+    <link rel="stylesheet" href="view/css/error404.css">
 </head>
 <body>
     <div class="container">
