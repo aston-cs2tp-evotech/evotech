0d8ab3cae74728f6978b164d7959c686c0e8ccfe
gurjotsd <gurjotsinghdhillon14@gmail.com>
2023-12-07 17:53:10 +0000

    finished  form, add new info, new review section

--- view/contactspage.php
+++ view/contactspage.php
@@ -13,30 +13,56 @@
 
     <section class="contact-container">
         <div class="contact-form">
+            <h2>Contact Form</h2> 
             <form id="contactForm">
-                <label for="name">Name:</label>
-                <input type="text" id="name" name="name" required>
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name" required>
 
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="email" required>
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" required>
 
-                <label for="description">Description:</label>
-                <textarea id="description" name="description" rows="4" required></textarea>
+    <label for="description">Description:</label>
+    <textarea id="description" name="description" rows="4" required></textarea>
 
-               
-                <button type="button" onclick="submitForm()">Submit</button>
-            </form>
+    <label for="date">Date:</label>
+    <input type="date" id="date" name="date" required>
+
+    <button type="submit">Submit</button>
+</form>
+
         </div>
 
         <div class="contact-info">
             <h2>Contact Information</h2>
             <p>Email: info@evotech.com</p>
-            <p>Phone: ??</p>
-            <p>Address: ??</p>
+            <p>Phone: 1234567891011</p>
+            <p>Address: Aston St, Birmingham B4 7ET</p>
         </div>
     </section>
 
-    <script src="script.js"></script>
+    <section class="customer-reviews-section">
+        <h2>Customer Reviews</h2>
+        <div class="customer-reviews">
+            <div class="review-box">
+                <img src="customer1.jpg" alt="Customer 1">
+                <p>Great service! EvoTech really helped us with our project. Highly recommended.</p>
+                <p>Date: 2023-01-01</p>
+            </div>
+
+            <div class="review-box">
+                <img src="customer2.jpg" alt="Customer 2">
+                <p>Responsive team and excellent communication. We are satisfied with their work.</p>
+                <p>Date: 2023-02-15</p>
+            </div>
+
+            <div class="review-box">
+                <img src="customer3.jpg" alt="Customer 3">
+                <p>EvoTech went above and beyond our expectations. Will definitely work with them again.</p>
+                <p>Date: 2023-03-30</p>
+            </div>
+        </div>
+    </section>
+
+    <script src="contacspage.js"></script>
 </body>
 </html>
-
