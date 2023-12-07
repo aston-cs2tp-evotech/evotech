8df21ec3214a08f808a2d3e6197cc1b62da0ef84
gurjotsd <gurjotsinghdhillon14@gmail.com>
2023-12-07 19:50:07 +0000

    view product page is been created

---
+++ view/productpage.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product Page</title>
+    <link rel="stylesheet" href="view/css/productpage.css">
+</head>
+
+<body>
+
+    <header>
+        <h1>Product Page</h1>
+    </header>
+
+    <main>
+        <div class="product-image">
+            <img src="product_image.jpg" alt="Product Image">
+        </div>
+
+        <div class="product-details">
+            <h2>Product Name</h2>
+            <p class="product-price">$99.99</p>
+            <p>text</p>
+
+            <div class="product-description">
+                <h3>Description</h3>
+                <p>Additional details about the product go here...</p>
+            </div>
+
+            <div class="add-to-basket">Add to Basket</div>
+        </div>
+    </main>
+
+</body>
+
+</html>
