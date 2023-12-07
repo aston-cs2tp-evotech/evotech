cde314441d4a367de2399b97d50c266c20fcc1ce
ToadallyStupid <toadallystupid@hotmail.com>
2023-12-07 17:49:40 +0000

    change func names to upper camel and register back - undid commit d021ebf - undid commit c0b9a5d - changed internal funcs to also use upper camel

--- controller/Controller.php
+++ controller/Controller.php
@@ -40,17 +40,17 @@ $Order = new OrdersModel($pdo);
  * @param mixed $var Variable to check
  * @return boolean True if $var is safe and exists, otherwise false
  */
-function checkExists($var) {
+function CheckExists($var) {
     return (isset($var) && !empty($var));
 }
 
 /** 
  * Puts all relevent user info into the global userInfo array
 */
-function reLogInUser() {
+function ReLogInUser() {
     global $userInfo, $Customer;
     //checks if it is set and not false
-    if (checkExists($_SESSION["uid"])) {
+    if (CheckExists($_SESSION["uid"])) {
         $uid = $_SESSION["uid"];
         //querys the database to get user info
         $userInfo = $Customer->getCustomerByUID($uid);
@@ -61,9 +61,9 @@ function reLogInUser() {
  * Checks if the user is logged in
  * @return boolean True if logged in, false if not
  */
-function checkLoggedIn() {
+function CheckLoggedIn() {
     global $userInfo;
-    return (checkExists($_SESSION["uid"]) && checkExists($userInfo));
+    return (CheckExists($_SESSION["uid"]) && CheckExists($userInfo));
 }
 
 /**
@@ -72,23 +72,23 @@ function checkLoggedIn() {
  * @param string $pass Customer's password
  * @return boolean True if login succeeded, otherwise false
  */
-function attemptLogin($user, $pass) {
+function AttemptLogin($user, $pass) {
     global $Customer;
-    if (!checkExists($user) || !(gettype($user) == "string")) return false;
-    if (!checkExists($pass) || !(gettype($pass) == "string")) return false;
+    if (!CheckExists($user) || !(gettype($user) == "string")) return false;
+    if (!CheckExists($pass) || !(gettype($pass) == "string")) return false;
     //attempts to fetch details via username
     $details = $Customer->getCustomerByUsername($user);
-    if (!checkExists($details)) {
+    if (!CheckExists($details)) {
         //falls back to fetching via email
         if (!filter_var($user, FILTER_VALIDATE_EMAIL)) return false;
         $details = $Customer->getCustomerByEmail($user);
-        if (!checkExists($details)) return false;
+        if (!CheckExists($details)) return false;
     }
     //checks passwords match
     if (password_verify($pass, $details["PasswordHash"])) {
         $_SESSION["uid"] = $details["CustomerID"];
         unset($details);
-        reLogInUser();
+        ReLogInUser();
         return true;
     }
     else return false;
@@ -99,13 +99,13 @@ function attemptLogin($user, $pass) {
  * @param array $details Associative array with relevent info (most likely just $_POST)
  * @return string Empty if succeeds (ie. evaluates to false), or a string to indicate where it failed
  */
-function registerUser($details) {
+function RegisterUser($details) {
     global $Customer;
-    if (!checkExists($details["email"]) || !filter_var($details["email"], FILTER_VALIDATE_EMAIL)) return "Invalid Email";
-    if (!checkExists($details["username"]) || !preg_match("/[a-zA-Z0-9]+/", $details["username"])) return "Invalid Username";
-    if (!checkExists($details["address"]) || !preg_match("/[a-zA-Z0-9.,]+/", $details["address"])) return "Invalid address";
-    if (!checkExists($details["password"]) || !(gettype($details["password"] == "string")) || !(strlen($details["password"]) > 7)) return "Invalid password";
-    if (!checkExists($details["password_confirmation"]) || !($details["password"] === $details["confirmpass"])) return "Confirmation password does not match";
+    if (!CheckExists($details["email"]) || !filter_var($details["email"], FILTER_VALIDATE_EMAIL)) return "Invalid Email";
+    if (!CheckExists($details["username"]) || !preg_match("/[a-zA-Z0-9]+/", $details["username"])) return "Invalid Username";
+    if (!CheckExists($details["customer_address"]) || !preg_match("/[a-zA-Z0-9.,]+/", $details["customer_address"])) return "Invalid address";
+    if (!CheckExists($details["password"]) || !(gettype($details["password"] == "string")) || !(strlen($details["password"]) > 7)) return "Invalid password";
+    if (!CheckExists($details["password_confirmation"]) || !($details["password"] === $details["confirmpass"])) return "Confirmation password does not match";
     if ($Customer->getCustomerByUsername($details["username"])) return "Username is already taken";
     if ($Customer->getCustomerByEmail($details["email"])) return "Email is already in use";
     //change field name for model
@@ -115,7 +115,7 @@ function registerUser($details) {
     if (!$Customer->registerUser($details)) return "Database Error";
     //if here, then success
     $_SESSION["uid"] = $Customer->getCustomerByUsername($details["username"])["CustomerID"];
-    reLogInUser();
+    ReLogInUser();
     return "";
 }
 
@@ -124,12 +124,12 @@ function registerUser($details) {
  * @param array $details Associative array containing field to change, new value and other relevant info
  * @return string Empty if succeeded, or a string to indicate where it failed
  */
-function updateCustomerDetail($details) {
+function UpdateCustomerDetail($details) {
     global $Customer;
     $details["field"] = strtolower($details["field"]);
     //preliminary checks
-    if (!checkLoggedIn()) return "Not logged in";
-    if (!checkExists($details["field"]) || !checkExists($details["value"]) || !(gettype($details["value"]) == "string")) return "Invalid request";
+    if (!CheckLoggedIn()) return "Not logged in";
+    if (!CheckExists($details["field"]) || !CheckExists($details["value"]) || !(gettype($details["value"]) == "string")) return "Invalid request";
 
     switch ($details["field"]) {
         case "username":
@@ -165,7 +165,7 @@ function updateCustomerDetail($details) {
 
         case "password":
             //password uses additional fields so those need checking too
-            if (!checkExists($details["newPassword"]) || !checkExists($details["confirmPassword"] ||  !(gettype($details["newPassword"] == "string")))) return "Invalid request";
+            if (!CheckExists($details["newPassword"]) || !CheckExists($details["confirmPassword"] ||  !(gettype($details["newPassword"] == "string")))) return "Invalid request";
             //checks that can be done before queries
             if (strlen($details["newPassword"]) < 7) return "New password should be longer than 7 characters";
             if ($details["newPassword"] !== $details["confirmPassword"]) return "New and confirmation passwords should match";
@@ -184,7 +184,7 @@ function updateCustomerDetail($details) {
 /**
  * Unsets both global arrays and destroys the session
  */
-function logOut() {
+function LogOut() {
     global $userInfo;
     unset($userInfo);
     unset($_SESSION);
@@ -208,12 +208,12 @@ function logOut() {
  * @param array $product Array to hold the product being checked
  * @return boolean True if succeeded, otherwise false
  */
-function productAndQuantityCheck($productID, $quantity, $product){
+function ProductAndQuantityCheck($productID, $quantity, $product){
     global $Product;
     //check legitimate quantity
-    if (!checkExists($quantity) || !(gettype($quantity) == "integer") || !($quantity >= 0)) return false;
+    if (!CheckExists($quantity) || !(gettype($quantity) == "integer") || !($quantity >= 0)) return false;
     //check PID is int (no SQL injection allowed here sorry)
-    if (!checkExists($productID) || !(gettype($productID) == "integer")) return false;
+    if (!CheckExists($productID) || !(gettype($productID) == "integer")) return false;
     //check product is legit
     $product = $Product->getProductByID($productID);
     if (!$product) return false;
@@ -229,13 +229,13 @@ function productAndQuantityCheck($productID, $quantity, $product){
  * @param int $quantity Quantity of specified product to add to basket
  * @return boolean True if succeeded, otherwise false
  */
-function addProductToBasket($productID, $quantity) {
+function AddProductToBasket($productID, $quantity) {
     global $Order;
     //check login
-    if (!checkLoggedIn()) return false;
+    if (!CheckLoggedIn()) return false;
     //init array for product
     $product = Array();
-    if (!productAndQuantityCheck($productID, $quantity, $product)) return false;
+    if (!ProductAndQuantityCheck($productID, $quantity, $product)) return false;
     //quantity needs additional check to make sure it isnt 0
     if ($quantity === 0) return false;
 
@@ -254,7 +254,7 @@ function addProductToBasket($productID, $quantity) {
     //check if product is already in basket
     $orderLines = $Order->getAllOrderLinesByOrderID($basket["OrderID"]);
     if (!$orderLines) return false;
-    foreach ($orderLines as $orderLine) if ($orderLine["ProductID"] == $productID) return modifyProductQuantityInBasket($productID, $orderLine["Quantity"]+$quantity);
+    foreach ($orderLines as $orderLine) if ($orderLine["ProductID"] == $productID) return ModifyProductQuantityInBasket($productID, $orderLine["Quantity"]+$quantity);
 
     //product not in basket, appending
     if ($Order->createOrderLine(Array("orderID"=>$basket["OrderID"], "productID"=>$productID, "quantity"=>$quantity))) {
@@ -269,13 +269,13 @@ function addProductToBasket($productID, $quantity) {
  * @param int $quantity New quantity
  * @return boolean True if succeeded, otherwise false
  */
-function modifyProductQuantityInBasket($productID, $quantity) {
+function ModifyProductQuantityInBasket($productID, $quantity) {
     global $Order;
     //check login
-    if (!checkLoggedIn()) return false;
+    if (!CheckLoggedIn()) return false;
     //init array for product
     $product = Array();
-    if (!productAndQuantityCheck($productID, $quantity, $product)) return false;
+    if (!ProductAndQuantityCheck($productID, $quantity, $product)) return false;
 
     //gets basket to update (also checks ownership)
     $basket = $Order->getAllOrdersByStatusNameAndCustomerID("Basket", $_SESSION["uid"]);
