9eb14419d54bbea709a942911cb10c506a1dc90b
ToadallyStupid <toadallystupid@hotmail.com>
2023-12-08 16:52:00 +0000

    all the changes to controller since 686b8fd

--- controller/Controller.php
+++ controller/Controller.php
@@ -1,6 +1,5 @@
 <?php
 
-
 //TODO: add function to check contact form data
 
 // ---------------------------------------------
@@ -15,11 +14,31 @@
 global $userInfo;
 
 /**
+ * @var PDO The database connection
+ */
+global $pdo;
+
+/**
+ * @var CustomerModel The customer model for interacting with the database
+ */
+$Customer = new CustomerModel($pdo);
+
+/**
+ * @var ProductModel The product model for interacting with the database
+ */
+$Product = new ProductModel($pdo);
+
+/**
+ * @var OrdersModel The orders model for interacting with the database
+ */
+$Order = new OrdersModel($pdo);
+
+/**
  * Check if a variable is safe to evaluate
  * @param mixed $var Variable to check
  * @return boolean True if $var is safe and exists, otherwise false
  */
-function checkExists($var) {
+function CheckExists($var) {
     return (isset($var) && !empty($var));
 }
 
@@ -29,7 +48,7 @@ function checkExists($var) {
 function ReLogInUser() {
     global $userInfo, $Customer;
     //checks if it is set and not false
-    if (checkExists($_SESSION["uid"])) {
+    if (CheckExists($_SESSION["uid"])) {
         $uid = $_SESSION["uid"];
         //querys the database to get user info
         $userInfo = $Customer->getCustomerByUID($uid);
@@ -42,7 +61,7 @@ function ReLogInUser() {
  */
 function CheckLoggedIn() {
     global $userInfo;
-    return (checkExists($_SESSION["uid"]) && checkExists($userInfo));
+    return (CheckExists($_SESSION["uid"]) && CheckExists($userInfo));
 }
 
 /**
@@ -53,12 +72,15 @@ function CheckLoggedIn() {
  */
 function AttemptLogin($user, $pass) {
     global $Customer;
+    if (!CheckExists($user) || !(gettype($user) == "string")) return false;
+    if (!CheckExists($pass) || !(gettype($pass) == "string")) return false;
     //attempts to fetch details via username
     $details = $Customer->getCustomerByUsername($user);
-    if (!checkExists($details)) {
+    if (!CheckExists($details)) {
         //falls back to fetching via email
+        if (!filter_var($user, FILTER_VALIDATE_EMAIL)) return false;
         $details = $Customer->getCustomerByEmail($user);
-        if (!checkExists($details)) return false;
+        if (!CheckExists($details)) return false;
     }
     //checks passwords match
     if (password_verify($pass, $details["PasswordHash"])) {
@@ -103,7 +125,7 @@ function UpdateCustomerDetail($details) {
     $details["field"] = strtolower($details["field"]);
     //preliminary checks
     if (!CheckLoggedIn()) return "Not logged in";
-    if (!checkExists($details["field"]) || !checkExists($details["value"])) return "Invalid request";
+    if (!CheckExists($details["field"]) || !CheckExists($details["value"]) || !(gettype($details["value"]) == "string")) return "Invalid request";
 
     switch ($details["field"]) {
         case "username":
@@ -139,10 +161,10 @@ function UpdateCustomerDetail($details) {
 
         case "password":
             //password uses additional fields so those need checking too
-            if (!checkExists($details["newPassword"]) || !checkExists($details["confirmPassword"])) return "Invalid request";
+            if (!CheckExists($details["newPassword"]) || !CheckExists($details["confirmPassword"] ||  !(gettype($details["newPassword"] == "string")))) return "Invalid request";
             //checks that can be done before queries
             if (strlen($details["newPassword"]) < 7) return "New password should be longer than 7 characters";
-            if ($details["newPassword"] != $details["confirmPassword"]) return "New and confirmation passwords should match";
+            if ($details["newPassword"] !== $details["confirmPassword"]) return "New and confirmation passwords should match";
             
             $user = $Customer->getCustomerByUID($_SESSION["uid"]);
             if (!password_verify($details["value"], $user["PasswordHash"])) return "Current password is incorrect";
@@ -166,5 +188,280 @@ function LogOut() {
     //route back to main page or page that responds that user logged out
 }
 
+// ---------------------------------------------
+//
+// FUNCTIONS RELATING TO PRODCUTS
+//
+// ---------------------------------------------
+
+/**
+ * Gets product from the database, regardless of stock
+ * @param int $productID ID of the product
+ * @return array|boolean Array with product details if success, otherwise false
+ */
+function GetProductByID($productID) {
+    global $Product;
+    if (!checkExists($productID) || !(gettype($productID) == "integer")) return false;
+    return $Product->getProductByID($productID);
+}
+
+/**
+ * --INTERNAL USE ONLY-- Filters array to only have stocked products
+ * @param array $products Array of products to filter (will overwrite)
+ * @return boolean True if success, otherwise false
+ */
+function FilterStockedProducts($products) {
+    if (!$products) return false;
+    $stockedProducts = array();
+    foreach ($products as $product) if ($product["Stock"] > 0) array_push($stockedProducts, $product);
+    if (!$stockedProducts) return false;
+    $products = $stockedProducts;
+    return true;
+}
+
+/**
+ * Gets every product in the database, regardless of stock
+ * @return array|boolean 2d array if succeeded, otherwise false
+ */
+function GetAllProducts() {
+    global $Product;
+    return $Product->getAllProducts();
+}
+
+/**
+ * Gets every product in the database where stock > 0
+ * @return array|boolean 2d array if succeeded, otherwise false
+ */
+function GetAllStockedProducts() {
+    $products = GetAllProducts();
+    if (!FilterStockedProducts($products)) return false;
+    else return $products;
+}
+
+/**
+ * --CURRENTLY DOES NOTHING-- Gets all products by category, regardless of stock
+ * @param string $category Category of the product (component, accessory etc.)
+ * @return array|boolean 2d array if succeeded, otherwise false
+ */
+function GetAllProductsByCategory($category){
+    global $Product;
+    //currently isn't implemented in db so this will just never return an array in the mean time
+    $categories = array("");
+    if (!CheckExists($category) || !(gettype($category) == "string") || !(in_array($category, $categories))) return false;
+    return false;
+}
+
+/**
+ * --CURRENTLY DOES NOTHING-- Gets all products by category where stock > 0
+ * @param string $category Category of the product (component, accessory etc)
+ * @return array|boolean 2d array if succeeded, otherwise false
+ */
+function GetAllStockedProductsByCategory($category) {
+    $products = GetAllProductsByCategory($category);
+    if (!FilterStockedProducts($products)) return false;
+    else return $products;
+}
+
+// ---------------------------------------------
+//
+// FUNCTIONS RELATING TO ORDERS 
+//
+// ---------------------------------------------
+
+
+/**
+ * --INTERNAL USE ONLY-- checks for product to make sure it's all legit
+ * @param int $productID PID of product
+ * @param int $quantity Quantity of product
+ * @param array $product Array to hold the product being checked
+ * @return boolean True if succeeded, otherwise false
+ */
+function ProductAndQuantityCheck($productID, $quantity, $product){
+    global $Product;
+    //check legitimate quantity
+    if (!CheckExists($quantity) || !(gettype($quantity) == "integer") || !($quantity >= 0)) return false;
+    //check PID is int (no SQL injection allowed here sorry)
+    if (!CheckExists($productID) || !(gettype($productID) == "integer")) return false;
+    //check product is legit
+    $product = $Product->getProductByID($productID);
+    if (!$product) return false;
+    //check product has enough stock
+    if ($product["Stock"] < $quantity) return false;
+    //passed all checks
+    return true;
+}
+
+/**
+ * Adds specified product to user's basket
+ * @param int $productID PID of the product
+ * @param int $quantity Quantity of specified product to add to basket
+ * @return boolean True if succeeded, otherwise false
+ */
+function AddProductToBasket($productID, $quantity) {
+    global $Order;
+    //check login
+    if (!CheckLoggedIn()) return false;
+    //init array for product
+    $product = array();
+    if (!ProductAndQuantityCheck($productID, $quantity, $product)) return false;
+    //quantity needs additional check to make sure it isnt 0
+    if ($quantity === 0) return false;
+
+    //create basket if none found, otherwise add item to it
+    //(also checks for ownership of basket)
+    $basket = $Order->getAllOrdersByOrderStatusNameAndCustomerID("basket", $_SESSION["uid"]);
+    $prodPrice = $product["Price"] * $quantity;
+    if (!$basket) {
+        //basket doesn't exist so it makes a new one
+        $orderStatID = $Order->getOrderStatusIDByName("basket");
+        if (!$orderStatID) return false;
+        $orderID = $Order->createOrder(array("customerID"=>$_SESSION["uid"], "totalAmount"=>$prodPrice,"orderStatusID"=>$orderStatID));
+        if (!$orderID) return false;
+        return $Order->createOrderLine(array("orderID"=>$orderID, "productID"=>$productID, "quantity"=>$quantity));
+    }
+    //check if product is already in basket
+    $orderLines = $Order->getAllOrderLinesByOrderID($basket["OrderID"]);
+    if (!$orderLines) return false;
+    foreach ($orderLines as $orderLine) if ($orderLine["ProductID"] == $productID) return ModifyProductQuantityInBasket($productID, $orderLine["Quantity"]+$quantity);
+
+    //product not in basket, appending
+    if ($Order->createOrderLine(array("orderID"=>$basket["OrderID"], "productID"=>$productID, "quantity"=>$quantity))) {
+        return $Order->updateOrderDetails($basket["OrderID"], "TotalAmount", $basket["TotalAmount"]+$prodPrice);
+    }
+    else return false;
+}
+
+/**
+ * Changes quantity of specified product in user's basket, a quantity of 0 will delete product from basket
+ * @param int $productID PID of the product
+ * @param int $quantity New quantity
+ * @return boolean True if succeeded, otherwise false
+ */
+function ModifyProductQuantityInBasket($productID, $quantity) {
+    global $Order;
+    //check login
+    if (!CheckLoggedIn()) return false;
+    //init array for product
+    $product = array();
+    if (!ProductAndQuantityCheck($productID, $quantity, $product)) return false;
+
+    //gets basket to update (also checks ownership)
+    $basket = $Order->getAllOrdersByOrderStatusNameAndCustomerID("basket", $_SESSION["uid"]);
+    if (!$basket) return false;
+    $allOrderLines = $Order->getAllOrderLinesByOrderID($basket["OrderID"]);
+    if (!$allOrderLines) return false;
+
+    //fetch orderLine with product in it
+    $orderLine = array();
+    foreach ($allOrderLines as $curOrderLine) if ($curOrderLine["ProductID"] == $productID) $orderLine = $curOrderLine;
+    if (!$orderLine) return false;
+
+    //check for orderLine deletion
+    if ($quantity == 0) {
+        if (!$Order->deleteOrderLine($basket["OrderID"], $product["ProductID"])) return false;
+        return $Order->updateOrderDetails($basket["OrderID"], "TotalAmount", $basket["TotalAmount"]-($orderLine["Quantity"]*$product["Price"]));
+    }
+
+    //change quantity and update price to match
+    $oldPrice = $orderLine["Quantity"] * $product["Price"];
+    $newPrice = $quantity * $product["Price"];
+    $newTotal = $basket["TotalAmount"] - $oldPrice + $newPrice;
+    if (!$Order->updateOrderLineDetails($basket["OrderID"], $product["ProductID"], "Quantity", $quantity)) return false;
+    return $Order->updateOrderDetails($basket["OrderID"], "TotalAmount", $newTotal);
+}
+
+/**
+ * Checks out the basket (if it exists), of the logged in customer
+ * @return boolean True if succeeded, otherwise false
+ */
+function CheckoutBasket() {
+    global $Order;
+    if (!CheckLoggedIn()) return false;
+    //fetch basket
+    $basket = $Order->getAllOrdersByOrderStatusNameAndCustomerID("basket", $_SESSION["uid"]);
+    if (!$basket) return false;
+
+    //passed all checks, updating status
+    return $Order->updateOrderDetails($basket["OrderID"], "OrderStatusID", $Order->getOrderStatusIDByName("ready"));
+}
+
+/**
+ * --INTERNAL USE ONLY-- Formats the passed orderlines to create an array with the order
+ * @param array $orderLines Array of OrderLines
+ * @param array $basket Associative array of the order (array[int][string])
+ * @return boolean True if succeeded, otherwise false
+ */
+function FormatOrderLines($orderLines, $basket) {
+    global $Product;
+
+    for ($i=0; $i<count($orderLines); $i++) {
+        $product = $Product->getProductByID($orderLines[$i]["ProductID"]);
+        if (!$product) return false;
+
+        $basket[$i]["ProductID"] = $product["ProductID"];
+        $basket[$i]["ProductName"] = $product["Name"];
+        $basket[$i]["Quantity"] = $orderLines[$i]["Quantity"];
+        $basket[$i]["TotalStock"] = $product["Stock"];
+        $basket[$i]["UnitPrice"] = $product["Price"];
+        $basket[$i]["TotalPrice"] = $basket[$i]["UnitPrice"] * $basket[$i]["Quantity"];
+    }
+    return true;
+}
+
+/**
+ * Retrieves the customer's basket
+ * @param string $totalAmount empty var when passed, holds total price for order if succeeds
+ * @return array|boolean 2d array (array[int][string]) if success, otherwise null
+ */
+function GetCustomerBasket($totalAmount) {
+    global $Order;
+    if (!CheckLoggedIn()) return false;
+
+    //retrieve order
+    $intOrder = $Order->getAllOrdersByOrderStatusNameAndCustomerID("basket", $_SESSION["uid"]);
+    if (!$intOrder) return false;
+    $intOrderLines = $Order->getAllOrderLinesByOrderID($intOrder["OrderID"]);
+    if (!$intOrderLines) return false;
+
+    //format return value
+    $basket = array(array());
+    if (!FormatOrderLines($intOrderLines, $basket)) return false;
+
+    //assign price and return
+    $totalAmount = $intOrder["TotalAmount"];
+    return $basket;
+
+}
+
+/**
+ * Retrieves all previous orders for a customer (not incl. basket)
+ * @param array $totalAmounts empty array when passed, each index holds the total amount for the respective order
+ * @return array|boolean 3d array (array[int][int][string]) if success, otherwise false
+ */
+function GetPreviousOrders($totalAmounts) {
+    global $Order;
+    if (!CheckLoggedIn()) return false;
+
+    //retrieve orders
+    $orders = $Order->getAllOrdersByOrderStatusNameAndCustomerID("delivered", $_SESSION["uid"]);
+    if (!$orders) return false;
+    
+    //retrieve all orderlines associated with each order
+    $orderLines = array(array());
+    for ($i=0; $i<count($orders); $i++) { 
+        $orderLines[$i] = $Order->getAllOrderLinesByOrderID($orders[$i]["OrderID"]);
+        if (!$orderLines[$i]) return false;
+    }
+
+    //format return value
+    $megaBasket = array(array(array()));
+    for ($i=0; $i<count($orderLines); $i++) if (!FormatOrderLines($orderLines[$i], $megaBasket[$i])) return false;
+
+    //get total amounts for each order
+    for ($i=0; $i<count($orders); $i++) $totalAmounts[$i] = $orders[$i]["TotalAmount"];
+
+    //return array
+    return $megaBasket;
+}
 
 ?>
