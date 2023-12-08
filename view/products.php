df172ad7be095e0a153e85b228b8a981c652d32f
aarond6504@gmail.com <aarond6504@gmail.com>
2023-12-08 20:46:48 +0000

    Added basic version of products page

---
+++ view/products.php
@@ -0,0 +1,343 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" type="text/css" href="Basket.css">
+    <title>EvoTech</title>
+    <ion-icon name="desktop-outline"></ion-icon>
+
+
+</head>
+<header>
+  
+</header>
+<body>
+ <?php include __DIR__ . '/nav.php'?>
+
+<!-- Box to prevent content being covered by navbar-->  
+<section class="bg p-5 py-4">
+    </section>
+    <section class="bg-dark text-light p-5 text-center text-sm-start py-4">
+      <div class="container">
+          <div class="d-sm-flex allign-items-center justify-content-between">
+              <div>
+                  <p class="lead my-3 text-center">
+                    Products
+                  </p>
+              </div> 
+          </div>
+      </div>
+  
+  </section>
+
+    <section class="Products">
+        <div class="shadow p-3 mb-5 bg-light body-tertiary rounded">
+            <div class="container">
+                <div class="row align-items-center">
+                  <div class="col">
+                    <div class="card" style="width: 25rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">Product name</p>
+                          <span>Price</span>
+                        </div>
+                      </div>
+                  </div>
+                  <div class="col">
+                    <div class="card" style="width: 25rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">Product name</p>
+                          <span>Price</span>
+                        </div>
+                      </div>
+                  </div>
+                  <div class="col">
+                    <div class="card" style="width: 25rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">Product name</p>
+                          <span>Price</span>
+                        </div>
+                      </div>
+                  </div>
+                </div>
+                <div class="row align-items-center">
+                  <div class="col">
+                    <div class="card" style="width: 25rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">Product name</p>
+                          <span>Price</span>
+                        </div>
+                      </div>
+                  </div>
+                  <div class="col">
+                    <div class="card" style="width: 25rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">Product name</p>
+                          <span>Price</span>
+                        </div>
+                      </div>
+                  </div>
+                  <div class="col">
+                    <div class="card" style="width: 25rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">Product name</p>
+                          <span>Price</span>
+                        </div>
+                      </div>
+                  </div>
+                </div>
+                <div class="row align-items-center">
+                  <div class="col">
+                    <div class="card" style="width: 25rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">Product name</p>
+                          <span>Price</span>
+                        </div>
+                      </div>
+                  </div>
+                  <div class="col">
+                    <div class="card" style="width: 25rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">Product name</p>
+                          <span>Price</span>
+                        </div>
+                      </div>
+                  </div>
+                  <div class="col">
+                    <div class="card" style="width: 25rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">Product name</p>
+                          <span>Price</span>
+                        </div>
+                      </div>
+                  </div>
+                </div>
+                <div class="row align-items-center">
+                  <div class="col">
+                    <div class="card" style="width: 25rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">Product name</p>
+                          <span>Price</span>
+                        </div>
+                      </div>
+                  </div>
+                  <div class="col">
+                    <div class="card" style="width: 25rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">Product name</p>
+                          <span>Price</span>
+                        </div>
+                      </div>
+                  </div>
+                  <div class="col">
+                    <div class="card" style="width: 25rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">Product name</p>
+                          <span>Price</span>
+                        </div>
+                      </div>
+                  </div>
+                </div>
+                <div class="row align-items-center">
+                  <div class="col">
+                    <div class="card" style="width: 25rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">Product name</p>
+                          <span>Price</span>
+                        </div>
+                      </div>
+                  </div>
+                  <div class="col">
+                    <div class="card" style="width: 25rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">Product name</p>
+                          <span>Price</span>
+                        </div>
+                      </div>
+                  </div>
+                  <div class="col">
+                    <div class="card" style="width: 25rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">Product name</p>
+                          <span>Price</span>
+                        </div>
+                      </div>
+                  </div>
+                </div>
+                <div class="row align-items-center">
+                  <div class="col">
+                    <div class="card" style="width: 25rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">Product name</p>
+                          <span>Price</span>
+                        </div>
+                      </div>
+                  </div>
+                  <div class="col">
+                    <div class="card" style="width: 25rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">Product name</p>
+                          <span>Price</span>
+                        </div>
+                      </div>
+                  </div>
+                  <div class="col">
+                    <div class="card" style="width: 25rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">Product name</p>
+                          <span>Price</span>
+                        </div>
+                      </div>
+                  </div>
+                </div>
+                <div class="row align-items-center">
+                  <div class="col">
+                    <div class="card" style="width: 25rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">Product name</p>
+                          <span>Price</span>
+                        </div>
+                      </div>
+                  </div>
+                  <div class="col">
+                    <div class="card" style="width: 25rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">Product name</p>
+                          <span>Price</span>
+                        </div>
+                      </div>
+                  </div>
+                  <div class="col">
+                    <div class="card" style="width: 25rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">Product name</p>
+                          <span>Price</span>
+                        </div>
+                      </div>
+                  </div>
+                </div>
+                <div class="row align-items-center">
+                  <div class="col">
+                    <div class="card" style="width: 25rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">Product name</p>
+                          <span>Price</span>
+                        </div>
+                      </div>
+                  </div>
+                  <div class="col">
+                    <div class="card" style="width: 25rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">Product name</p>
+                          <span>Price</span>
+                        </div>
+                      </div>
+                  </div>
+                  <div class="col">
+                    <div class="card" style="width: 25rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">Product name</p>
+                          <span>Price</span>
+                        </div>
+                      </div>
+                  </div>
+                </div>
+                <div class="row align-items-center">
+                  <div class="col">
+                    <div class="card" style="width: 25rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">Product name</p>
+                          <span>Price</span>
+                        </div>
+                      </div>
+                  </div>
+                  <div class="col">
+                    <div class="card" style="width: 25rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">Product name</p>
+                          <span>Price</span>
+                        </div>
+                      </div>
+                  </div>
+                  <div class="col">
+                    <div class="card" style="width: 25rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">Product name</p>
+                          <span>Price</span>
+                        </div>
+                      </div>
+                  </div>
+                </div>
+                <div class="row align-items-center">
+                  <div class="col">
+                    <div class="card" style="width: 25rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">Product name</p>
+                          <span>Price</span>
+                        </div>
+                      </div>
+                  </div>
+                  <div class="col">
+                    <div class="card" style="width: 25rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">Product name</p>
+                          <span>Price</span>
+                        </div>
+                      </div>
+                  </div>
+                  <div class="col">
+                    <div class="card" style="width: 25rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">Product name</p>
+                          <span>Price</span>
+                        </div>
+                      </div>
+                  </div>
+                </div>
+
+
+
+
+
+
+
+              </div>
+        </div>
+    </section>
+</body>
+
+</html>
