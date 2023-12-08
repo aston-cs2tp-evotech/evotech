3c70982be7c4f4963419361dfdf42490b585516c
gurjotsd <gurjotsinghdhillon14@gmail.com>
2023-12-08 12:20:27 +0000

    added recommended products

--- view/productpage.php
+++ view/productpage.php
@@ -15,21 +15,56 @@
     </header>
 
     <main>
-        <div class="product-image">
-            <img src="product_image.jpg" alt="Product Image">
-        </div>
-
-        <div class="product-details">
-            <h2>Product Name</h2>
-            <p class="product-price">$99.99</p>
-            <p>text</p>
-
-            <div class="product-description">
-                <h3>Description</h3>
-                <p>Additional details about the product go here...</p>
+        <div class="product-box">
+            <div class="product-image">
+                <img src="product_image.jpg" alt="Product Image">
             </div>
 
-            <div class="add-to-basket">Add to Basket</div>
+            <div class="product-details">
+                <h2>Product Name</h2>
+                <p class="product-price">$99.99</p>
+                <p>text</p>
+
+                <div class="product-description">
+                    <h3>Description</h3>
+                    <p>Additional details about the product go here...</p>
+                </div>
+
+                <div class="add-to-basket">Add to Basket</div>
+            </div>
+        </div>
+
+        <!-- Recommended Products -->
+        <div class="recommended-products">
+            <div class="product-box">
+                <div class="product-image">
+                    <img src="recommended_product1.jpg" alt="Recommended Product 1">
+                </div>
+
+                <div class="product-details">
+                    <h2>Recommended Product 1</h2>
+                </div>
+            </div>
+
+            <div class="product-box">
+                <div class="product-image">
+                    <img src="recommended_product2.jpg" alt="Recommended Product 2">
+                </div>
+
+                <div class="product-details">
+                    <h2>Recommended Product 2</h2>
+                </div>
+            </div>
+
+            <div class="product-box">
+                <div class="product-image">
+                    <img src="recommended_product3.jpg" alt="Recommended Product 3">
+                </div>
+
+                <div class="product-details">
+                    <h2>Recommended Product 3</h2>
+                </div>
+            </div>
         </div>
     </main>
 
