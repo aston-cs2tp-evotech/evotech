17d73e8cf1f64d43ee509b09fe29518c903504c5
ToadallyStupid <toadallystupid@hotmail.com>
2023-12-08 15:36:49 +0000

    Merge branch 'main' into authFunctions

---
+++ view/productpage.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product Page</title>
+    <link rel="stylesheet" href="view/css/productpage.css">
+</head>
+
+<body>
+
+    <header>
+        <h1>Product Page</h1>
+    </header>
+
+    <main>
+        <div class="product-box">
+            <div class="product-image">
+                <img src="product_image.jpg" alt="Product Image">
+            </div>
+
+            <div class="product-details">
+                <h2>Product Name</h2>
+                <p class="product-price">price</p>
+                <p>text</p>
+
+                <div class="product-description">
+                    <h3>Description</h3>
+                    <p>Additional details about the product go here...</p>
+                </div>
+
+                <div class="add-to-basket">Add to Basket</div>
+            </div>
+        </div>
+
+        <div class="recommended-products">
+            <div class="product-box">
+                <div class="product-image">
+                    <img src="recommended_product1.jpg" alt="Recommended Product 1">
+                </div>
+
+                <div class="product-details">
+                    <h2>Recommended Product 1</h2>
+                </div>
+            </div>
+
+            <div class="product-box">
+                <div class="product-image">
+                    <img src="recommended_product2.jpg" alt="Recommended Product 2">
+                </div>
+
+                <div class="product-details">
+                    <h2>Recommended Product 2</h2>
+                </div>
+            </div>
+
+            <div class="product-box">
+                <div class="product-image">
+                    <img src="recommended_product3.jpg" alt="Recommended Product 3">
+                </div>
+
+                <div class="product-details">
+                    <h2>Recommended Product 3</h2>
+                </div>
+            </div>
+        </div>
+    </main>
+
+</body>
+
+</html>
