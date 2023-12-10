187bd6b408f5189d62b817accbe5b6adcc60762f
YourKalamity <kalam@kalam.dev>
2023-12-10 16:08:23 +0000

    Make customer display past orders

--- view/customer.php
+++ view/customer.php
@@ -1,67 +1,112 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+global $userInfo;
+if (isset($_SESSION["uid"])) {
+    ReLogInUser();
+}
+
+// Check if Username is set in $userInfo and then set $username
+if (isset($userInfo["Username"])) {
+    $username = $userInfo["Username"];
+}
+
+// Get previous orders
+$totalAmount = array();
+$orders = GetPreviousOrders($totalAmount); // Assuming $orders is a 3D array: $orders[order][product]['KEY']
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Customer</title>
+    <title>Customer Details</title>
     <!-- Link to Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <!-- Link to custom CSS -->
-    <link rel="stylesheet" type="text/css" href="/view/css/login_register_checkout_customer.css"/>
+    <!-- Link to your custom CSS -->
+    <link rel="stylesheet" type="text/css" href="/view/css/login_register_checkout_customer.css">
 </head>
 
 <body>
+    <?php include __DIR__ . '/nav.php' ?>
     <header class="bg-dark text-white text-center py-4">
-        <h1>Customer</h1>
+        <h1><br>Welcome, <?php echo $username; ?>!</h1>
     </header>
 
-    <main>
-        <section class="Customer">
-        <div class="shadow p-3 mb-5 bg-light body-tertiary rounded">
-            <div class="container">
-            <div class="card mb-3">
-            <div class="no no-gutters">
-            <div class="col-md-4">
-            <h2>Change password</h2>
-            <form action="change_password" method="POST">
-                <label for="password">Enter a new password</label>
-                <br>
-                <input type="password" name="password" placeholder="Password" required/>
-                <br>
-                <br>
-                <label for="confirmpass">Re-enter password</label>
-                <br>
-                <input id="password_confirmation" type="password" name="confirmpass" placeholder="Confirm password" required/>
-                <br>
-                <br>
-                <input class="btn btn-primary" type="submit" value="Change password"/>
-            </form>
-            <br>
-            <br>
-            <h2>Change address</h2>
-            <form action="change_address" method="POST">
-                <label for="customer_address">Enter new address</label>
-                <br>
-                <textarea name="customer_address" placeholder="Address" rows="6" required></textarea>
-                <br>
-                <br>
-                <input class="btn btn-primary" type="submit" value="Change address"/>
-            </form>
-            <br>
-            <br>
-            <h2>Your past orders</h2>
+    <main class="container mt-4">
+        <div class="row">
+            <div class="col-lg-6">
+                <h2>Customer Details</h2>
+                <p><strong>Username:</strong> <?php echo $userInfo["Username"]; ?></p>
+                <p><strong>Email:</strong> <?php echo $userInfo["Email"]; ?></p>
+                <p><strong>Address:</strong> <?php echo $userInfo["CustomerAddress"]; ?></p>
             </div>
-            </div>
-            </div>
+
+            <div class="col-lg-6">
+                <h2>Change Password</h2>
+                <!-- Form to change password -->
+                <form action="/change-password" method="POST">
+                    <div class="form-group">
+                        <label for="current_password">Current Password</label>
+                        <input type="password" class="form-control" name="current_password" required/>
+                    </div>
+                    <div class="form-group">
+                        <label for="new_password">New Password</label>
+                        <input type="password" class="form-control" name="new_password" required/>
+                    </div>
+                    <div class="form-group">
+                        <label for="confirm_password">Confirm New Password</label>
+                        <input type="password" class="form-control" name="confirm_password" required/>
+                    </div>
+                    <input type="submit" class="btn btn-primary" value="Change Password"/>
+                </form>
             </div>
         </div>
-        </section>
+
+        <div class="mt-4">
+            <h2>Past Orders</h2>
+            <?php if (!empty($orders)) : ?>
+                <?php $orders = array_reverse($orders, true); // Reverse the order of the array ?>
+                <?php foreach ($orders as $orderID => $order) : ?>
+                    <?php $totalPrice = 0; ?>
+                    <h3>Order <?php echo $orderID; ?></h3>
+                    <h4>Status: <?php echo $order[0]['Status']; ?></h4>
+                    <div class="table-responsive">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>Product</th>
+                                    <th>Quantity</th>
+                                    <th>Subtotal</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($order as $product) : ?>
+                                    <?php $totalPrice += $product['UnitPrice'] * $product['Quantity']; ?>
+                                    <tr>
+                                        <td><?php echo $product['ProductName']; ?></td>
+                                        <td><?php echo $product['Quantity']; ?></td>
+                                        <td>£<?php echo $product['UnitPrice'] * $product['Quantity']; ?></td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                        <h3 class="text-right">Total: £<?php echo $totalPrice; ?></h3>
+                    </div>
+                <?php endforeach; ?>
+            <?php else : ?>
+                <p>No past orders found.</p>
+            <?php endif; ?>
+        </div>
     </main>
 
     <!-- Link to Bootstrap JS -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
 </body>
 
 </html>
-
-                    
