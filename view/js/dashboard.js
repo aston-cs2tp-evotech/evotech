41fc57ce49996612d70260463ccd4690d1abfaac
YourKalamity <kalam@kalam.dev>
2024-03-14 21:43:15 +0000

    Remember last dashboard page visited

--- view/js/dashboard.js
+++ view/js/dashboard.js
@@ -1,3 +1,4 @@
+// Function to show the page based on the ID
 function showPage(pageId) {
   // Hide all pages
   var pages = document.getElementsByClassName("page");
@@ -18,5 +19,24 @@ function showPage(pageId) {
   // Add 'active' class to the clicked navigation link
   var clickedNavLink = document.querySelector('a[href="#"][onclick="showPage(\'' + pageId + '\')"]');
   clickedNavLink.classList.add("active");
+
+  // Store the current page ID in localStorage
+  localStorage.setItem("currentPage", pageId);
 }
 
+// Function to retrieve and show the last visited page on page load
+function showLastVisitedPage() {
+  // Retrieve the last visited page ID from localStorage
+  var lastVisitedPage = localStorage.getItem("currentPage");
+  
+  // If a last visited page is found, show it
+  if (lastVisitedPage) {
+    showPage(lastVisitedPage);
+  } else {
+    // Default to showing the dashboard if no last visited page is found
+    showPage("dashboard");
+  }
+}
+
+// Call the function to show the last visited page on page load
+showLastVisitedPage();
