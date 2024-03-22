2cfcbd21bfe837f14dab8cee065134c5d165cfb5
gurjotsd <gurjotsinghdhillon14@gmail.com>
2024-03-22 15:24:39 +0000

    test

--- setup/example_config.php
+++ setup/example_config.php
@@ -1,12 +1,12 @@
 <?php
 // The username for the database connection
-$db_username = "your_username"; // Default: "your_username"
+$db_username = "root"; // Default: "your_username"
 
 // The password for the database connection
-$db_password = "your_password"; // Default: "your_password"
+$db_password = ""; // Default: "your_password"
 
 // The name of the database to connect to
-$db_database_name = "your_database_name"; // Default: "your_database_name"
+$db_database_name = "root_db"; // Default: "your_database_name"
 
 // The server where the database is hosted
 $db_server = "localhost"; // Default: "localhost"
