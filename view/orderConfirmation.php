79aeea56d34799c4dcd51f697dc02bd897974c8b
Hanzalah Naguthane <220070614@aston.ac.uk>
2024-03-24 21:50:37 +0000

    added order faillure and success pages and css

--- view/orderConfirmation.php
+++ view/orderConfirmation.php
@@ -22,26 +22,28 @@ if (isset($userInfo)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Order Confirmation - EvoTech</title>
+    <title>Order Confirmation - Evotech</title>
     <!-- Link to Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <!-- Link to your custom CSS -->
-    <link rel="stylesheet" type="text/css" href="/view/css/login_register_checkout.css"/>
+    <link rel="stylesheet" type="text/css" href="/view/css/orderConfirmation.css"/>
 </head>
 
 <body>
     <?php include __DIR__ . '/nav.php'?>
-    <header class="bg-dark text-white text-center py-4">
-        <h1><br>Order Confirmation</h1>
-    </header>
+
+    <div class="container" style= "background-color: #A499B3">
+        <header class="mt-3">
+            <h1>Order Confirmation</h1>
+        </header>
 
     <main>
         <div class="container mt-4">
             <div class="row">
                 <div class="col-lg-12">
-                    <h2>Your order has been confirmed</h2>
-                    <p>Thank you, <?php echo $username; ?>, for your order!</p>
-                    <a href="/customer" class="btn btn-primary">View account and order history</a>
+                    <h2>Your Order Has Been Confirmed</h2>
+                    <p>Thank you <?php echo $username; ?>, for Your Order!</p>
+                    <a href="/customer" class="btn btn-primary">View Account and Order History</a>
                 </div>
             </div>
         </div>
